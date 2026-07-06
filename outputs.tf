3cbc6063b0a0fa978b374f54d4aae85c3f9f190c
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:15:52 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,23 @@
 output "system_center_virtual_machine_manager_virtual_machine_instance_guest_agents" {
   description = "All system_center_virtual_machine_manager_virtual_machine_instance_guest_agent resources"
   value       = azurerm_system_center_virtual_machine_manager_virtual_machine_instance_guest_agent.system_center_virtual_machine_manager_virtual_machine_instance_guest_agents
+  sensitive   = true
+}
+output "system_center_virtual_machine_manager_virtual_machine_instance_guest_agents_password" {
+  description = "List of password values across all system_center_virtual_machine_manager_virtual_machine_instance_guest_agents"
+  value       = [for k, v in azurerm_system_center_virtual_machine_manager_virtual_machine_instance_guest_agent.system_center_virtual_machine_manager_virtual_machine_instance_guest_agents : v.password]
+  sensitive   = true
+}
+output "system_center_virtual_machine_manager_virtual_machine_instance_guest_agents_provisioning_action" {
+  description = "List of provisioning_action values across all system_center_virtual_machine_manager_virtual_machine_instance_guest_agents"
+  value       = [for k, v in azurerm_system_center_virtual_machine_manager_virtual_machine_instance_guest_agent.system_center_virtual_machine_manager_virtual_machine_instance_guest_agents : v.provisioning_action]
+}
+output "system_center_virtual_machine_manager_virtual_machine_instance_guest_agents_scoped_resource_id" {
+  description = "List of scoped_resource_id values across all system_center_virtual_machine_manager_virtual_machine_instance_guest_agents"
+  value       = [for k, v in azurerm_system_center_virtual_machine_manager_virtual_machine_instance_guest_agent.system_center_virtual_machine_manager_virtual_machine_instance_guest_agents : v.scoped_resource_id]
+}
+output "system_center_virtual_machine_manager_virtual_machine_instance_guest_agents_username" {
+  description = "List of username values across all system_center_virtual_machine_manager_virtual_machine_instance_guest_agents"
+  value       = [for k, v in azurerm_system_center_virtual_machine_manager_virtual_machine_instance_guest_agent.system_center_virtual_machine_manager_virtual_machine_instance_guest_agents : v.username]
 }
 
