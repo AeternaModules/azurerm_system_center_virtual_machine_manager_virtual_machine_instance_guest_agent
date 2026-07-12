c19967bb49f897b446cec240ce761b09e1a06470
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:32:28 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "system_center_virtual_machine_manager_virtual_machine_instance_guest_agents_id" {
+  description = "Map of id values across all system_center_virtual_machine_manager_virtual_machine_instance_guest_agents, keyed the same as var.system_center_virtual_machine_manager_virtual_machine_instance_guest_agents"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_virtual_machine_instance_guest_agent.system_center_virtual_machine_manager_virtual_machine_instance_guest_agents : k => v.id }
+}
 output "system_center_virtual_machine_manager_virtual_machine_instance_guest_agents_password" {
   description = "Map of password values across all system_center_virtual_machine_manager_virtual_machine_instance_guest_agents, keyed the same as var.system_center_virtual_machine_manager_virtual_machine_instance_guest_agents"
   value       = { for k, v in azurerm_system_center_virtual_machine_manager_virtual_machine_instance_guest_agent.system_center_virtual_machine_manager_virtual_machine_instance_guest_agents : k => v.password }
