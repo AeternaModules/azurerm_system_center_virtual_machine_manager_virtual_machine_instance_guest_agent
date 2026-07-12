abeb8ea7890e7abd83b1d75f2dd8d8b983b49fbc
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:59:41 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
+data "azurerm_key_vault_secret" "password" {
+  for_each     = { for k, v in var.system_center_virtual_machine_manager_virtual_machine_instance_guest_agents : k => v if v.password_key_vault_id != null && v.password_key_vault_secret_name != null }
+  name         = each.value.password_key_vault_secret_name
+  key_vault_id = each.value.password_key_vault_id
+}
 resource "azurerm_system_center_virtual_machine_manager_virtual_machine_instance_guest_agent" "system_center_virtual_machine_manager_virtual_machine_instance_guest_agents" {
   for_each = var.system_center_virtual_machine_manager_virtual_machine_instance_guest_agents
 
-  password            = each.value.password
+  password            = each.value.password != null ? each.value.password : try(data.azurerm_key_vault_secret.password[each.key].value, null)
   scoped_resource_id  = each.value.scoped_resource_id
   username            = each.value.username
   provisioning_action = each.value.provisioning_action
--- variables.tf
+++ variables.tf
@@ -3,6 +3,8 @@ variable "system_center_virtual_machine_manager_virtual_machine_instance_guest_a
 Map of system_center_virtual_machine_manager_virtual_machine_instance_guest_agents, attributes below
 Required:
     - password
+    - password_key_vault_id (alternative to password - read from Key Vault instead)
+    - password_key_vault_secret_name (alternative to password - read from Key Vault instead)
     - scoped_resource_id
     - username
 Optional:
@@ -10,10 +12,12 @@ Optional:
 EOT
 
   type = map(object({
-    password            = string
-    scoped_resource_id  = string
-    username            = string
-    provisioning_action = optional(string) # Default: "install"
+    password                       = string
+    password_key_vault_id          = optional(string)
+    password_key_vault_secret_name = optional(string)
+    scoped_resource_id             = string
+    username                       = string
+    provisioning_action            = optional(string) # Default: "install"
   }))
 }
 
