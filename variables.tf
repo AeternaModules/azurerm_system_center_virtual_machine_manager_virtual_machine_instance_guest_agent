b8ec9cb3dffbad1774aeaf03a10e0f23144ec4d2
Daniel Allen <dan@aeternagroup.net>
2026-07-13 19:39:23 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -3,8 +3,8 @@ variable "system_center_virtual_machine_manager_virtual_machine_instance_guest_a
 Map of system_center_virtual_machine_manager_virtual_machine_instance_guest_agents, attributes below
 Required:
     - password
-    - password_key_vault_id (alternative to password - read from Key Vault instead)
-    - password_key_vault_secret_name (alternative to password - read from Key Vault instead)
+    - password_key_vault_id (optional, alternative to password)
+    - password_key_vault_secret_name (optional, alternative to password)
     - scoped_resource_id
     - username
 Optional:
@@ -17,7 +17,7 @@ EOT
     password_key_vault_secret_name = optional(string)
     scoped_resource_id             = string
     username                       = string
-    provisioning_action            = optional(string) # Default: "install"
+    provisioning_action            = optional(string)
   }))
 }
 
